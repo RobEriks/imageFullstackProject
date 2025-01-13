771fffc2b3f3dc5efe95de3d1652e81c98bace9e
RobEriks <Rob.Eriksson@outlook.com>
2025-01-13 22:47:50 +0100

    U

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use('/api', imageRoutes);
 
 // Databas connect
 mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
