fa679107b2f295afd26ad5522378e371f3ba455f
RobEriks <Rob.Eriksson@outlook.com>
2025-01-15 22:37:14 +0100

    U

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(cors());
 
 app.use('/api', imageRoutes);
 
-// Databas connect
+// Databas connection with env
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
